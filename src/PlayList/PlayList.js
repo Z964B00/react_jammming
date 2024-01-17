274439d80598e857d9ea048b74762b8b80340d73
Zach Brown <z.964b00@gmail.com>
2024-01-16 21:35:01 -0500

    Hard coded playlist

--- src/PlayList/PlayList.js
+++ src/PlayList/PlayList.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import TrackList from "../TrackList/TrackList";
 
 import "./PlayList.css";
 
-const PlayList= () => {
+const PlayList= (props) => {
+
+    const [ playlistTitle, setPlaylistTitle ] = useState("");
+    const [ playlistTitleColor, setPlaylistTitleColor ] = useState("grey")
+
+    const [ playlistTracks, setPlaylistTracks ] = useState([
+        { name: "Track 4", artist: "Artist 4", album: "Album 4", id: 4 },
+        { name: "Track 5", artist: "Artist 5", album: "Album 5", id: 5 },
+        { name: "Track 6", artist: "Artist 6", album: "Album 6", id: 6 }
+    ]);
+
+    const handleTitleChange = (event) => {
+        const newTitle = event.target.value;
+        setPlaylistTitle(newTitle);
+        setPlaylistTitleColor(newTitle ? "black" : "grey");
+    };
+
+    const handleInputClick = () => {
+        if (playlistTitle === "") {
+            setPlaylistTitle("");
+            setPlaylistTitleColor("black");
+        }
+    }
+
     return (
         <div className="Playlist">
-            <h2>Playlist</h2>
-            <TrackList />
+            <div class="PlaylistTitle">
+                <input 
+                    type="text"
+                    placeholder="My Playlist Title"
+                    value={playlistTitle}
+                    onChange={handleTitleChange}
+                    onClick={handleInputClick}
+                    style={{ color:playlistTitleColor }} />
+            </div>
+            <TrackList tracks={playlistTracks} />
             <button>Save to Spotify</button>
         </div>
     );
--- src/SearchResults/SearchResults.js
+++ src/SearchResults/SearchResults.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./SearchResults.css";
 import TrackList from "../TrackList/TrackList";
 
 const SearchResults = () => {
-    const trackArray = [
+    const [ searchResults, setSearchResults ] = useState([
         { name: "Track 1", artist: "Artist 1", album: "Album 1", id: 1 },
         { name: "Track 2", artist: "Artist 2", album: "Album 2", id: 2 },
         { name: "Track 3", artist: "Artist 3", album: "Album 3", id: 3 },
         { name: "Track 4", artist: "Artist 4", album: "Album 4", id: 4 },
         { name: "Track 5", artist: "Artist 5", album: "Album 5", id: 5 }
-    ];
+    ]);
+
+    const updateSearchResults = (newResults) => {
+        setSearchResults(newResults);
+    };
 
     return (
      <div className="SearchResults">
         <h2>Results</h2>
-        <TrackList tracks={trackArray} />
+        <TrackList tracks={searchResults} updateSearchResults={updateSearchResults} />
      </div>  
     );
 };
