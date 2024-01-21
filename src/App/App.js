a05cc47991943e6450e0de5b0de3794b5fb2d5de
Zach Brown <z.964b00@gmail.com>
2024-01-21 10:57:05 -0500

    Added uris to tracks

--- src/App/App.js
+++ src/App/App.js
@@ -7,20 +7,20 @@ import PlayList from "../PlayList/PlayList";
 
 function App() {
   const [ searchResults, setSearchResults ] = useState([
-    { name: "Track 1", artist: "Artist 1", album: "Album 1", id: 1 },
-    { name: "Track 2", artist: "Artist 2", album: "Album 2", id: 2 },
-    { name: "Track 3", artist: "Artist 3", album: "Album 3", id: 3 },
-    { name: "Track 4", artist: "Artist 4", album: "Album 4", id: 4 },
-    { name: "Track 5", artist: "Artist 5", album: "Album 5", id: 5 }
+    { name: "Track 1", artist: "Artist 1", album: "Album 1", id: 1, uri:"spotify:track:1rqhFgbbKwnb9MLmUQDhG1" },
+    { name: "Track 2", artist: "Artist 2", album: "Album 2", id: 2, uri:"spotify:track:2rqhFgbbKwnb9MLmUQDhG2" },
+    { name: "Track 3", artist: "Artist 3", album: "Album 3", id: 3, uri:"spotify:track:3rqhFgbbKwnb9MLmUQDhG3" },
+    { name: "Track 4", artist: "Artist 4", album: "Album 4", id: 4, uri:"spotify:track:4rqhFgbbKwnb9MLmUQDhG4" },
+    { name: "Track 5", artist: "Artist 5", album: "Album 5", id: 5, uri:"spotify:track:5rqhFgbbKwnb9MLmUQDhG5" }
   ]);
 
   const [ playlistTitle, setPlaylistTitle ] = useState("");
   const [ playlistTitleColor, setPlaylistTitleColor ] = useState("grey")
 
   const [ playlistTracks, setPlaylistTracks ] = useState([
-    { name: "Track 4", artist: "Artist 4", album: "Album 4", id: 4 },
-    { name: "Track 5", artist: "Artist 5", album: "Album 5", id: 5 },
-    { name: "Track 6", artist: "Artist 6", album: "Album 6", id: 6 }
+    { name: "Track 6", artist: "Artist 6", album: "Album 6", id: 6, uri:"spotify:track:6rqhFgbbKwnb9MLmUQDhG6" },
+    { name: "Track 7", artist: "Artist 7", album: "Album 7", id: 7, uri:"spotify:track:7rqhFgbbKwnb9MLmUQDhG7" },
+    { name: "Track 8", artist: "Artist 8", album: "Album 8", id: 8, uri:"spotify:track:8rqhFgbbKwnb9MLmUQDhG8" }
   ]);
 
 
